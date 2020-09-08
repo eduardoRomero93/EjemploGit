a222b0fdb83e7ab9b5e43b313b6379a7a059ff0a
Eduardo Romero <za17012130@zapopan.tecmm.edu.mx>
2020-09-08 18:24:04 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Mi numero de control es 17012130')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Mario Eduardo Villalobos Romero')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
